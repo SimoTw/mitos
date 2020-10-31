058e1a2d21e28089db52de8a81ce4c0fd5b7a586
simo <simohsiehtw@gmail.com>
2020-10-31 23:20:48 +0800

    primes test passed

--- user/primes.c
+++ user/primes.c
@@ -2,65 +2,61 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-#define R 0
-#define W 1
+
+void redirect(int fd, int fds[]) {
+    close(fd);
+    dup(fds[fd]);
+    close(fds[0]);
+    close(fds[1]);    
+}
+
+void source() {
+    int i;
+    for (i = 2; i <= 35; i++)
+        write(1, &i, sizeof(i));
+    close(1);
+    
+}
+
+void parent_process(int parent_pds[]) {
+    int prime;
+    if (read(0, &prime, sizeof(prime))) {
+        printf("prime %d\n", prime);
+    } else {
+        exit(0);
+    }
+    int child_pds[2];
+    pipe(child_pds);
+    int pid;
+    pid = fork();
+    if (pid > 0) {
+        int num;
+        redirect(1, child_pds);
+        while(read(0, &num, sizeof(num)))
+            if (num % prime != 0) 
+                write(1, &num, sizeof(num));
+        close(0);
+        close(1);
+        wait((int *)0);
+    } else if (pid == 0) {
+        redirect(0, child_pds);
+        parent_process(child_pds);
+
+    }
+}
+
 
 int main(int argc, char *argv[]) {
-    int first_pipe[2];
-    int second_pipe[2];
-    int *left_pipe, *right_pipe;
-    int i, num, prime, pid, n;
-    int *temp;
-
-    pipe(first_pipe);
-    if (fork() > 0) {
-        close(first_pipe[R]);
-        for(i = 2; i <= 35; i++) {
-            write(first_pipe[W], &i, sizeof(i));
-        }
-        write(first_pipe[W], "\0", 1);
-        close(first_pipe[W]);
-        printf("exfore wait");
-
+    int parent_pds[2];
+    pipe(parent_pds);
+    if (fork() > 0) { 
+        redirect(1, parent_pds);
+        source();
         wait((int *)0);
-        printf("after wait");
-
     } else {
-        pipe(second_pipe);
-        left_pipe = first_pipe;
-        right_pipe = second_pipe;
-        for(;;) {
-            pid = fork();
-            if (pid == 0) {
-                close(left_pipe[1]);
-                close(right_pipe[0]);
-                n = read(left_pipe[0], &prime, sizeof(prime));
-                if (n <= 1) {
-                 exit(0);
-
-                }
-                printf("prime %d\n", prime);
-                for (;;) {
-                    n = read(left_pipe[0], &num, sizeof num);
-                    if (n <= 1) {
-                        write(right_pipe[1], "\0", 1);
-                        break;
-                    }
-                    if (num % prime != 0) {
-                        write(right_pipe[1], &num, sizeof num);
-                    }
-                }
-                close(left_pipe[0]);
-                close(right_pipe[1]);
-                exit(0);
-            } else {
-                wait((int *)0);
-                temp = left_pipe;
-                left_pipe = right_pipe;
-                right_pipe = temp;
-            }
-        }
+        redirect(0, parent_pds);
+        parent_process(parent_pds);
+        wait(0);
     }
     exit(0);
-
 }
