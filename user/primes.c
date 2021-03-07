e28f85bea925aeb7c7ac674bba1280eacc625985
simo <simohsiehtw@gmail.com>
2021-03-07 22:35:27 +0800

    Update primes.

--- user/primes.c
+++ user/primes.c
@@ -2,60 +2,60 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-
 void redirect(int fd, int fds[]) {
     close(fd);
     dup(fds[fd]);
     close(fds[0]);
-    close(fds[1]);    
+    close(fds[1]);
 }
 
-void source() {
-    int i;
-    for (i = 2; i <= 35; i++)
-        write(1, &i, sizeof(i));
-    close(1);
-    
-}
-
-void parent_process(int parent_pds[]) {
-    int prime;
-    if (read(0, &prime, sizeof(prime))) {
-        printf("prime %d\n", prime);
-    } else {
-        exit(0);
-    }
-    int child_pds[2];
-    pipe(child_pds);
+void primeProcess(int pfds[]) {
+    int buf[1];
+    int p;
+    int fds[2];
     int pid;
-    pid = fork();
-    if (pid > 0) {
-        int num;
-        redirect(1, child_pds);
-        while(read(0, &num, sizeof(num)))
-            if (num % prime != 0) 
-                write(1, &num, sizeof(num));
+    // read p
+    redirect(0, pfds);
+    if (read(0, buf, sizeof(int)) == 0) {
         close(0);
+        exit(0);
+    } else {
+        p = buf[0];
+        fprintf(1, "prime %d\n", p);
+    }
+    pipe(fds);
+    pid = fork();
+    if (pid == 0) {
+        // create a child process to read the pile
+        primeProcess(fds);
+    } else {
+        // write primes to pipe
+        redirect(1, fds);
+        while (read(0, buf, 1) != 0) {
+            int num = buf[0];
+            if (num % p != 0)
+                write(1, &num, sizeof(num));
+        }
         close(1);
-        wait((int *)0);
-    } else if (pid == 0) {
-        redirect(0, child_pds);
-        parent_process(child_pds);
-
+        wait(0);
+        close(0);
     }
 }
 
-
-int main(int argc, char *argv[]) {
-    int parent_pds[2];
-    pipe(parent_pds);
-    if (fork() > 0) { 
-        redirect(1, parent_pds);
-        source();
-        wait((int *)0);
+int main(void) {
+    int i;
+    int pid;
+    int fds[2];
+    pipe(fds);
+    pid = fork();
+    if (pid == 0) {
+         primeProcess(fds);
     } else {
-        redirect(0, parent_pds);
-        parent_process(parent_pds);
+        redirect(1, fds);
+        for (i = 2; i <= 35; i++) {
+            write(1, &i,  sizeof(i));
+        }
+        close(1);
         wait(0);
     }
     exit(0);
